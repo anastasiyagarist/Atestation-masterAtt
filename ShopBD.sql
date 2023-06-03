fb71bbdef64781057aeedcb4e877344c26c027a2
anastasiyagarist <130681529+anastasiyagarist@users.noreply.github.com>
2023-06-03 23:16:49 +0300

    Add files via upload

---
+++ ShopBD.sql
@@ -0,0 +1,13 @@
+-- Database: ShopBD
+
+-- DROP DATABASE IF EXISTS "ShopBD";
+
+CREATE DATABASE "ShopBD"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
